412e88878aea4ac9b6360c2d70f6049d6700ec5e
Bharathkumar V <bharathkumar.vaitheeswaran@adp.com>
2016-03-28 07:46:52 +0530

    Initial Commit

---
+++ NodejsKing/src/routes/bookRoutes.js
@@ -0,0 +1,65 @@
+﻿var express = require('express');
+
+var bookRouter = express.Router();
+
+var router = function (nav){
+    var books = [{
+            title: 'War and Peace',
+            genre: 'Historical Fiction',
+            author: 'Lev Nikolayevich Tolstoy',
+            read: false
+        },
+        {
+            title: 'Les Miserables',
+            genre: 'Historical Fiction',
+            author: 'Victor Hugo',
+            read: false
+        },
+        {
+            title: 'The Time Machine',
+            genre: 'Science Fiction',
+            author: 'H.G.Wells',
+            read: false
+        },
+        {
+            title: 'A Journey into the Center of th Earth',
+            genre: 'Science Fiction',
+            author: 'Jules Verne',
+            read: false
+        },
+        {
+            title: 'The Dark World',
+            genre: 'Fantasy',
+            author: 'Henry Kuttner',
+            read: false
+        },
+        {
+            title: 'Adventures of IT Leader',
+            genre: 'Fantasy',
+            author: 'Henry Kuttner',
+            read: false
+        }];
+    
+    bookRouter.route('/')
+    .get(function (req, res) {
+        res.render('bookListView', {
+            title: 'Books', 
+            nav: nav,
+            books: books
+        });
+    });
+    
+    bookRouter.route('/:id')
+    .get(function (req, res) {
+        var id = req.params.id;
+        res.render('bookView', {
+            title: 'Book', 
+            nav: nav,
+            book: books[id]
+        });
+    });
+
+    return bookRouter;
+}
+
+module.exports = router;
